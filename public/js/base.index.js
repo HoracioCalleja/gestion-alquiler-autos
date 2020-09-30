a1d43787607a65c06297b0938dca6e915225b03d
Horacio Calleja <callejah@hotmail.com>
2020-09-30 18:44:29 -0300

    Update base.index.js

--- public/js/base.index.js
+++ public/js/base.index.js
@@ -1,11 +1,1 @@
-const d = document,
-  $autos = d.querySelector(".autos"),
-  $clientes = d.querySelector(".clientes");
 
-  if(window.location.href === "http://localhost:8080/cliente"){
-    $autos.classList.remove("is-active");
-    $clientes.classList.add("is-active");
-  } else if(window.location.href === "http://localhost:8080/auto" || window.location.href === "http://localhost:8080/"){
-    $clientes.classList.remove("is-active");
-    $autos.classList.add("is-active");
-  }
